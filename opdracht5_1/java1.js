ec245aa3cd6c5d822c696c1616360248f190f9ec
koen <9023005@student.zadkine.nl>
2023-10-05 09:23:31 +0200

    e

---
+++ opdracht5_1/java1.js
@@ -0,0 +1,12 @@
+function changeColor(kleur)
+{
+    console.log("Hello world!");
+    document.getElementById("kop1").style.color = kleur;
+    let result =funtion.repeat(5);
+}
+
+
+function verandertext()
+{
+    document.getElementById("kop1").innerHTML = "Inhoud aangepast!";
+}
---
+++ opdracht6_4/opdracht6_4.js
@@ -0,0 +1,17 @@
+function myFunction(kleur) 
+{
+    console.log("Hello world!");
+    document.getElementById("p1").style.color = kleur;
+
+  }
+  function verandertext()
+{
+    console.log("Hello world!");
+    document.getElementById("p2").innerHTML = "Inhoud aangepast!";
+}
+
+function calculate(getal1,getal2)
+{
+    antwoord = getal1 + getal2;
+    console.log(antwoord);
+}
