5297188ace05065acd63a8782fecd37ebdf4e8de
koen <9023005@student.zadkine.nl>
2023-10-25 10:45:45 +0200

    opdracht7_3

---
+++ opdracht7_3/opdracht7_3.js
@@ -0,0 +1,18 @@
+let getal = 0;
+let getalp = document.getElementById("waarde");
+
+
+function getaloptellen() {
+
+
+
+    if (getal >= 10 ){
+        getal = 0;
+
+
+
+    } else {
+      getal++;
+    }
+    getalp.innerHTML = getal.toString();
+  }
